e93d1d410584a13906541dc8293a7d02044ee9ea
vugvr617 <nasraddinli@digitale-lernumgebung.de>
2023-03-01 14:47:07 +0100

    Firebase fixed

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const config = { 
-    apiKey: "AIzaSyD-Hcj-v2YrTOCJ0zN85uJsAXpx-8R3_ZE",
-    authDomain: "instagram-clone-project-79ba1.firebaseapp.com",
-    projectId: "instagram-clone-project-79ba1",
-    storageBucket: "instagram-clone-project-79ba1.appspot.com",
-    messagingSenderId: "16045624068",
-    appId: "1:16045624068:web:d6df2b09f49c45e2245e51"
-}
+const config = {
+  apiKey: "AIzaSyD-Hcj-v2YrTOCJ0zN85uJsAXpx-8R3_ZE",
+  authDomain: "instagram-clone-project-79ba1.firebaseapp.com",
+  projectId: "instagram-clone-project-79ba1",
+  storageBucket: "instagram-clone-project-79ba1.appspot.com",
+  messagingSenderId: "16045624068",
+  appId: "1:16045624068:web:d6df2b09f49c45e2245e51",
+};
 
 const firebaseApp = firebase.initializeApp(config);
 

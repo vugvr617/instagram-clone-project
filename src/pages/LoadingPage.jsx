b6e60e95c368e38b66c2345e1809896bec0543c6
vugvr617 <vnasraddinli@gmail.com>
2023-03-03 01:53:37 +0100

    LoadingPage has been created

---
+++ src/pages/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingPage = () => {
+  return (
+    <div className="h-screen flex w-screen items-center justify-center">
+      <img className="max-w-[100px]" src="/images/instagram-logo.png" />
+    </div>
+  );
+};
+
+export default LoadingPage;
--- src/router.js
+++ src/router.js
@@ -1,25 +1,16 @@
 import React, { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
+import LoadingPage from "./pages/LoadingPage";
 
-const Login = lazy(() =>
-  new Promise((resolve) =>
-    setTimeout(() => resolve(import("./pages/Login")), 2000)
-  )
-);
+const Login = lazy(() => import("./pages/Login"));
 
 const router = createBrowserRouter([
   { path: "/", errorElement: ErrorPage },
   {
     path: "/login",
     element: (
-      <Suspense
-        fallback={
-          <div>
-            <p>Loading...</p>
-          </div>
-        }
-      >
+      <Suspense fallback={<LoadingPage />}>
         <Login />
       </Suspense>
     ),

cee66b8d5ede60325eff012273396543c7c59a92
vugvr617 <vnasraddinli@gmail.com>
2023-03-03 01:10:09 +0100

    React-Hook-Forms integrated to the login page

--- src/components/Login/LoginSection.jsx
+++ src/components/Login/LoginSection.jsx
@@ -2,30 +2,42 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const LoginSection = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const { register, watch, handleSubmit } = useForm();
 
-  const onSubmitEvent = (data) => {
+  const onSubmitFunction = (data) => {
     console.log(data.email);
   };
 
+  const password = watch("password");
+  const email = watch("email");
+
+  let isValid = !email || !password || password.length < 6;
+
   return (
     <form
-      onSubmit={handleSubmit(onSubmitEvent)}
+      onSubmit={handleSubmit(onSubmitFunction)}
       className="flex gap-2 w-[100%] flex-col items-center"
     >
       <input
         type="email"
         className="login-input"
         placeholder="Your email"
-        {...register("email", {required: true})}
+        {...register("email", { required: true })}
       />
       <input
         type="password"
         className="login-input"
         placeholder="Password"
-        {...register("password", {required: true, minLength: 6})}
+        {...register("password", { required: true, minLength: 6 })}
       />
-      <button className="text-sm font-bold bg-[#4DB5F9] text-[#fafafa] mt-2 w-[100%] h-[35px] rounded">
+      <button
+        disabled={!isValid}
+        className={`text-sm font-bold ${
+          isValid
+            ? "cursor-default bg-[#4DB5F9]"
+            : "cursor-pointer bg-[#0095f6]"
+        } text-[#fafafa] mt-2 w-[100%] h-[35px] rounded`}
+      >
         Log in
       </button>
     </form>
--- src/components/Login/RegisterSection.jsx
+++ src/components/Login/RegisterSection.jsx
@@ -1,16 +1,59 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const RegisterSection = () => {
+  const { register, handleSubmit, watch } = useForm();
+
+  const onSubmitFunction = () => {};
+  const username = watch("username");
+  const email = watch("email");
+  const password = watch("password");
+  const fullName = watch("fullName");
+
+  const isValid = !fullName || password.length < 6 || !email || !username;
+
+  console.log(isValid);
+
   return (
-    <div className="flex mt-[-15px] flex-col w-[100%] gap-2 items-center">
+    <form
+      onSubmit={handleSubmit(onSubmitFunction)}
+      className="flex mt-[-15px] flex-col w-[100%] gap-2 items-center"
+    >
       <p className="text-center">Sign up now!</p>
-      <input type="text" className="login-input" placeholder="Email" />
-      <input type="text" className="login-input" placeholder="Full name" />
-      <input type="text" className="login-input" placeholder="Password" />
-      <button className="text-sm font-bold bg-[#4DB5F9] text-[#fafafa] mt-2 w-[100%] h-[35px] rounded">
+      <input
+        type="text"
+        className="login-input"
+        placeholder="Email"
+        {...register("email", { required: true })}
+      />
+      <input
+        type="text"
+        className="login-input"
+        placeholder="Full name"
+        {...register("fullName", { required: true })}
+      />
+      <input
+        type="text"
+        className="login-input"
+        placeholder="Username"
+        {...register("username", { required: true })}
+      />
+      <input
+        type="password"
+        className="login-input"
+        placeholder="Password"
+        {...register("password", { required: true })}
+      />
+      <button
+        className={`text-sm font-bold ${
+          isValid
+            ? "cursor-default bg-[#4DB5F9]"
+            : "cursor-pointer bg-[#0095f6]"
+        } text-[#fafafa] mt-2 w-[100%] h-[35px] rounded`}
+      >
         Sign up
       </button>
-    </div>
+    </form>
   );
 };
 
